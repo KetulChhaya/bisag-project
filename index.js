d6761b26d97bdf91b9f7c04c825a62a1558f9e05
KetulChhaya <chhayaketul@gmail.com>
2022-11-23 23:27:24 +0530

    res

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // app.use("/api/user", userRoutes);
 // app.use("/api/general/", generalRoutes);
 // app.use("/api/pdf", pdfRoutes);
-app.use("/cloud", cloudinaryRoute);
+// app.use("/cloud", cloudinaryRoute);
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
